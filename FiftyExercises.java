460d6f50c0d9438d0517531fe4fbe87ae5e101fe
Nicholas Owens <nicholas.s.owens01@gmail.com>
2023-09-08 16:23:53 -0500

    exercise 4

--- FiftyExercises.java
+++ FiftyExercises.java
@@ -24,8 +24,16 @@ public class FiftyExercises {
         return num1/num2;
     }
 
+    static int problems() {
+        System.out.println(-5 + 8 * 6);
+        System.out.println((55+9)%9);
+        System.out.println(20+-3*5/8);
+        System.out.println(5+15/3*2-8%3);
+        return 0;
+    }
+
     public static void main(String[] args) {
-        System.out.println(divide());
+    problems();
     }
 
 }
