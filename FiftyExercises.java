13532b60465bf7964fba782d0932982845eaf918
Nicholas Owens <nicholas.s.owens01@gmail.com>
2023-09-08 16:18:55 -0500

    exercise 3

--- FiftyExercises.java
+++ FiftyExercises.java
@@ -17,8 +17,15 @@ public class FiftyExercises {
         return num1 + num2;
     }
 
-    public static void main(String[] args) {
+    static int divide() {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        return num1/num2;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(divide());
     }
 
 }
