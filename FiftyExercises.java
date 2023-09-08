5bc172e59d643f607675080d89394508359e45a0
Nicholas Owens <nicholas.s.owens01@gmail.com>
2023-09-08 16:15:22 -0500

    exercise 2

--- FiftyExercises.java
+++ FiftyExercises.java
@@ -1,16 +1,24 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class FiftyExercises {
 
     static String hello() {
-//        use String.format to put the given name into the string.
+//        Open a scanner object, use String.format to put the given name into the string.
         Scanner scanner = new Scanner(System.in);
         String name = scanner.next();
         return String.format("Hello \n%s", name);
     }
 
+    static int sum() {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        return num1 + num2;
+    }
+
     public static void main(String[] args) {
-        System.out.println(hello());
+
     }
 
 }
